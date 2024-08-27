fe4d3f37fae74c4bc470993692c9a13438e1554f
Umesh Prajapati <Umesh.Prajapati@godigit.com>
2024-08-27 20:33:34 +0530

    Extra code removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlsLoginComponent } from './pages/pls-login/pls-login.component';
 import { CardComponent } from './components/card/card.component';
-const routes: Routes = [
-  {
-    path: 'pls-login',
-    component: PlsLoginComponent,
-  },
-  {
-    path:'',
-    component:CardComponent,
-  }
-  
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

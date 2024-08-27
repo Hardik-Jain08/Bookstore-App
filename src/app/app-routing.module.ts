3bd9f84ab9d45c118bf9670ce05ae8493f1acbe4
Umesh Prajapati <Umesh.Prajapati@godigit.com>
2024-08-27 20:42:32 +0530

    resolved conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PlsLoginComponent } from './pages/pls-login/pls-login.component';
-import { CardComponent } from './components/card/card.component';
+
 const routes: Routes = [];
 
 @NgModule({
